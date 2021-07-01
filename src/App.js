c124411f46af10a63facc4353c58113d885ee126
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2021-07-01 15:38:14 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import HeaderImage from './images/portfolio-image.png';
 import Navbar from './components/Navbar/Navbar';
 import Contact from './components/Contact/Contact.js';
 import Home from './components/Home/Home.js';
