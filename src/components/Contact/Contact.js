be26c5957fd11d6e747e47d6c448915cb9c4e2f7
mirage <mirage.kamran.pkt@gmail.com>
2021-07-12 14:27:21 -0700

    contact form sytels updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,41 +1,44 @@
 import React from "react";
-import "../../styles.css";
+import "./Contact.css";
 
 import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
 
 const Contact = (props) => {
-  return (
-    <Form>
-      {props.touched.username && props.errors.username && (
-        <p className="error">{props.errors.username}</p>
-      )}
-      <Field type="text" name="username" placeholder="User Name" />
-      {props.touched.email && props.errors.email && <p>{props.errors.email}</p>}
-      <Field type="email" name="email" placeholder="Email" />
-      {props.touched.message && props.errors.message && (
-        <p>{props.errors.message}</p>
-      )}
-      <Field component="textarea" name="message" placeholder="Message" />
+    return (
+        <Form className="contact">
+            {props.touched.name && props.errors.name && (
+                <p className="error">{props.errors.name}</p>
+            )}
+            <Field type="text" name="name" placeholder="Name" />
+            {props.touched.email && props.errors.email && (
+                <p>{props.errors.email}</p>
+            )}
+            <Field type="email" name="email" placeholder="Email" />
+            {props.touched.message && props.errors.message && (
+                <p>{props.errors.message}</p>
+            )}
+            <Field component="textarea" name="message" placeholder="Message" />
 
-      <button type="submit">Submit</button>
-    </Form>
-  );
+            <button className="send-msg-btn" type="submit">
+                Submit
+            </button>
+        </Form>
+    );
 };
 
 export default withFormik({
-  mapPropsToValues: (props) => {
-    return {
-      username: props.username || "",
-      email: props.email || "",
-      message: props.message || "",
-    };
-  },
-  validationSchema: yup.object().shape({
-    username: yup.string().required("Please enter your username"),
-    email: yup.string().required("Please enter a valid email "),
-    message: yup.string().required("Please enter your message"),
-  }),
-  handleSubmit: (props) => {},
+    mapPropsToValues: (props) => {
+        return {
+            name: props.name || "",
+            email: props.email || "",
+            message: props.message || "",
+        };
+    },
+    validationSchema: yup.object().shape({
+        name: yup.string().required("Please enter your name"),
+        email: yup.string().required("Please enter a valid email"),
+        message: yup.string().required("Please enter your message"),
+    }),
+    handleSubmit: (props) => {},
 })(Contact);
-
